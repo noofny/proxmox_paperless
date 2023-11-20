2103d6455834dfd0e72446468f27e0abf4675891
noofny@gmail.com <noofny@gmail.com>
2023-11-20 14:23:49 +1100

    ...

--- setup_paperless.sh
+++ setup_paperless.sh
@@ -12,15 +12,6 @@ source /etc/default/locale
 update-locale ${LOCALE_VALUE}
 
 
-# # user/groups...
-# passwd
-# sudo addgroup --system docker
-# sudo adduser $USER docker
-# newgrp docker
-# sudo snap disable docker
-# sudo snap enable docker
-
-
 echo "Creating folders..."
 mkdir -p /home/docsadmin/paperless/broker
 mkdir -p /home/docsadmin/paperless/db
@@ -30,15 +21,15 @@ echo "Creating stack..."
 mv /docker-compose.yaml /home/docsadmin/docker-compose.yaml
 cd /home/docsadmin
 docker-compose pull
-echo "Creating user..."
-docker-compose run --rm webserver createsuperuser
+# echo "Creating user..."
+# docker-compose run --rm webserver createsuperuser
 echo "Starting stack..."
 docker-compose up --detach
 
 
-# echo "Connecting to container to setup user..."
-# echo "NOTE: once connected to interactive session run 'python manage.py createsuperuser', follow prompts, then exit"
-# docker exec -it paperless-webserver-1 bash
+echo "Connecting to container to setup user..."
+echo "NOTE: once connected to interactive session run 'python manage.py createsuperuser', follow prompts, then exit"
+docker exec -it paperless-webserver-1 bash
 
 
 echo "You can access the console at http://$(hostname -I):8000/"
